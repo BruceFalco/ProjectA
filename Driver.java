a7afb6ad08dce0ae2ece5ce7bbb94ccb672c326a
timon.luo <tjieluo.capri@gmail.com>
2015-01-09 09:27:20 -0500

    project

---
+++ Driver.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Driver extends JFrame {
+    
+    public Driver {
+
+	//add the board here
+
+	setTitle("The Best Freaking Game Ever To Exist On All
+ Planes of Existence");
+	setSize(600,600);
+	setLocationRelativeTo(null);
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+
+    public static void main(String[] args) {
+	Driver ex = new Driver();
+	ex.setVisible(true);
+    }
+}
+
